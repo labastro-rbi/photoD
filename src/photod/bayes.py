461f328f2fbab3e816756791376841ccdeb6b8c7
Sandro Campos <scampos@andrew.cmu.edu>
2024-10-21 16:30:58 +0200

    Upload minimal code and notebooks

---
+++ src/photod/bayes.py
@@ -0,0 +1,218 @@
+import photod.locus as lt
+import numpy as np
+
+from photod.priors import getBayesConstants, readPriors, getPriorMapIndex, make3Dprior
+from photod.others import getMargDistr3D, getStats, Entropy
+from photod.plotting import showCornerPlot3, showMargPosteriors3D, showQrCornerPlot, show3Flat2Dmaps
+
+def makeBayesEstimates3D(catalog, fitColors, locusData, locus3DList, ArGridList, priorsRootName, outfile, \
+                                iStart=0, iEnd=-1, myStars=[], verbose=False, MrColumn='Mr'):
+    
+    if (iEnd < iStart):
+        iStart = 0
+        iEnd = np.size(catalog)
+     
+    # read maps with priors (and interpolate on the Mr-FeH grid given by locusData, which is same for all stars)
+    priorGrid = readPriors(rootname=priorsRootName, locusData=locusData, MrColumn=MrColumn)
+    # get prior map indices using observed r band mags
+    priorind = getPriorMapIndex(catalog['rmag'])
+              
+    # properties of Ar grid for prior and likelihood
+    bc = getBayesConstants()
+    ArCoeff = {}
+    ArCoeff[0] = bc['ArCoeff0']
+    ArCoeff[1] = bc['ArCoeff1'] 
+    ArCoeff[2] = bc['ArCoeff2']
+    ArCoeff[3] = bc['ArCoeff3']
+    ArCoeff[4] = bc['ArCoeff4']
+
+    # color corrections due to dust reddening (for each Ar in the grid for this particular star) 
+    # for finding extinction, too
+    C = lt.extcoeff()
+    reddCoeffs = {}
+    reddCoeffs['ug'] = C['u']-C['g']
+    reddCoeffs['gr'] = C['g']-C['r']
+    reddCoeffs['ri'] = C['r']-C['i']
+    reddCoeffs['iz'] = C['i']-C['z']
+
+    # Mr and FeH 1-D grid properties extracted from locus data (same for all stars)
+    xLabel = 'FeH'
+    yLabel = 'Mr'
+    FeHGrid = locusData[xLabel]
+    MrGrid = locusData[yLabel]
+    FeH1d = np.sort(np.unique(FeHGrid))
+    Mr1d = np.sort(np.unique(MrGrid))
+    
+    # grid step
+    dFeH = FeH1d[1]-FeH1d[0]
+    dMr = Mr1d[1]-Mr1d[0]
+    # metadata for plotting likelihood maps below
+    FeHmin = np.min(FeH1d)
+    FeHmax = np.max(FeH1d)
+    FeHNpts = FeH1d.size  
+    MrFaint = np.max(Mr1d)
+    MrBright = np.min(Mr1d)
+    MrNpts = Mr1d.size 
+    mdLocus = np.array([FeHmin, FeHmax, FeHNpts, MrFaint, MrBright, MrNpts])
+
+    print('Mr1d=', np.min(Mr1d), np.max(Mr1d), len(Mr1d))
+    print('MrBright, MrFaint=', MrBright, MrFaint)
+    
+    # setup arrays for holding results
+    catalog['MrEst'] = 0.0*catalog['Mr'] - 99 
+    catalog['MrEstUnc'] = 0.0*catalog['Mr'] -1 
+    catalog['FeHEst'] = 0.0*catalog['Mr'] - 99
+    catalog['FeHEstUnc'] = 0.0*catalog['Mr'] - 1 
+    catalog['ArEst'] = 0.0*catalog['Mr'] - 99 
+    catalog['ArEstUnc'] = 0.0*catalog['Mr'] -1 
+    catalog['QrEst'] = 0.0*catalog['Mr'] - 99 
+    catalog['QrEstUnc'] = 0.0*catalog['Mr'] -1 
+    catalog['chi2min'] = 0.0*catalog['Mr'] - 99
+    catalog['MrdS'] = 0.0*catalog['Mr'] - 1 
+    catalog['FeHdS'] = 0.0*catalog['Mr'] - 1 
+    catalog['ArdS'] = 0.0*catalog['Mr'] - 1 
+
+    ### maximum grid values for Ar from master locus 
+    ArGridSmallMax = np.max(ArGridList['ArSmall'])
+    ArGridMediumMax = np.max(ArGridList['ArMedium'])
+
+    #######################################################################
+    ## loop over all stars (can be trivially parallelized) 
+    for i in range(iStart, iEnd):
+        if (verbose):
+            if (int(i/10000)*10000 == i): 
+                print('working on star', i)
+
+        ######## NEED TO SORT ALL INPUT AND WRITE & CALL makeLikelihoodCube()         
+        # chi2min, likeCube = makeLikelihoodCube(...inputs...)    
+        # input: i, catalog, ArCoeff, 
+
+        ### produce likelihood array for this star, using provided locus color model 
+
+        ### THIS BLOCK USES A LIST OF THREE 3D MODEL LOCII, with 3 different resolutions for Ar
+        # note that here true Ar is used (catalog['Ar'][i]); for real stars, need to use SFD or another extinction map
+        # ArMax = ArCoeff[0]*catalog['Ar'][i] + ArCoeff[1]
+        # ArMin = ArCoeff[3]*catalog['Ar'][i] + ArCoeff[4]
+
+        ArMax = ArCoeff[0]*catalog['Ar'].iloc[i] + ArCoeff[1]
+        ArMin = ArCoeff[3]*catalog['Ar'].iloc[i] + ArCoeff[4]
+        
+        # depending on ArMax, pick the adequate Ar resolution of locus3D
+        if (ArMax < ArGridSmallMax):
+            ArGrid = ArGridList['ArSmall']
+            locus3D = locus3DList['ArSmall']
+            # print('selected ArSmall locus3D with', len(locus3D), ' elements')
+        else:
+            if (ArMax < ArGridMediumMax):
+                ArGrid = ArGridList['ArMedium']
+                locus3D = locus3DList['ArMedium']
+                # print('selected ArMedium locus3D with', len(locus3D), ' elements')
+            else:
+                ArGrid = ArGridList['ArLarge']
+                locus3D = locus3DList['ArLarge']
+                # print('selected ArLarge locus3D with', len(locus3D), ' elements')
+        # subselect from chosen 3D locus (simply to have fewer Ar points and thus faster execution) 
+        Ar1d = ArGrid[(ArGrid>=ArMin)&(ArGrid<=ArMax)]
+
+        # simply limit by maximum plausible extinction Ar
+        # print('trying to select from locus3D with', len(locus3D), ' elements, ArMax=', ArMax)
+        locus3Dok = locus3D[(locus3D['Ar']>=ArMin)&(locus3D['Ar']<=ArMax)]
+        # print('selected locus3Dok with', len(locus3Dok), ' elements, from locus3D with', len(locus3D))
+
+        ##### the main Bayes block      
+        ## compute chi2 map using provided 3D model locus (locus3Dok) ##
+        # return i, fitColors, reddCoeffs, catalog, locus3Dok, ArCoeff, FeH1d, Mr1d, Ar1d
+        chi2map = lt.getPhotoDchi2map3D(i, fitColors, reddCoeffs, catalog, locus3Dok, ArCoeff, masterLocus=True)
+        ## likelihood map
+        likeGrid = np.exp(-0.5*chi2map)
+        # print('likeGrid:', likeGrid.shape)
+        likeCube = likeGrid.reshape(np.size(FeH1d), np.size(Mr1d), np.size(Ar1d))
+        # print('likeCube:', likeCube.shape)
+        #############################################################################################
+        
+        if (Ar1d.size>1):
+            dAr = Ar1d[1] - Ar1d[0]
+        else:
+            dAr = 0.01
+        catalog['chi2min'][i] = np.min(chi2map)
+        
+        ## generate 3D (Mr, FeH, Ar) prior from 2D (Mr, FeH) prior using uniform prior for Ar    
+        prior2d = priorGrid[priorind[i]].reshape(np.size(FeH1d), np.size(Mr1d))
+        priorCube = make3Dprior(prior2d, np.size(Ar1d))        
+        ## posterior data cube
+        postCube = priorCube * likeCube
+        
+        ## process to get expectation values and uncertainties
+        # marginalize and get stats
+        margpostMr = {}
+        margpostFeH = {}
+        margpostAr = {}
+        margpostMr[0], margpostFeH[0], margpostAr[0] = getMargDistr3D(priorCube, dMr, dFeH, dAr)
+        margpostMr[1], margpostFeH[1], margpostAr[1] = getMargDistr3D(likeCube, dMr, dFeH, dAr) 
+        margpostMr[2], margpostFeH[2], margpostAr[2] = getMargDistr3D(postCube, dMr, dFeH, dAr) 
+
+        # stats
+        catalog['MrEst'][i], catalog['MrEstUnc'][i] = getStats(Mr1d, margpostMr[2])
+        catalog['FeHEst'][i], catalog['FeHEstUnc'][i] = getStats(FeH1d, margpostFeH[2])
+        catalog['ArEst'][i], catalog['ArEstUnc'][i] = getStats(Ar1d, margpostAr[2])
+        catalog['MrdS'][i] = Entropy(margpostMr[2]) - Entropy(margpostMr[0])
+        catalog['FeHdS'][i] = Entropy(margpostFeH[2]) - Entropy(margpostFeH[0])
+        catalog['ArdS'][i] = Entropy(margpostAr[2]) - Entropy(margpostAr[0])
+
+        # for testing and illustration  
+        if (i in myStars):
+            # plot 
+            FeHStar = catalog['FeH'][i]
+            MrStar = catalog['Mr'][i]
+            ArStar = catalog['Ar'][i]
+            indA = np.argmax(margpostAr[2])
+            show3Flat2Dmaps(priorCube[:,:,indA], likeCube[:,:,indA], postCube[:,:,indA], mdLocus, xLabel, yLabel, logScale=True, x0=FeHStar, y0=MrStar)
+            showMargPosteriors3D(Mr1d, margpostMr, 'Mr', 'p(Mr)', FeH1d, margpostFeH, 'FeH', 'p(FeH)', Ar1d, margpostAr, 'Ar', 'p(Ar)', MrStar, FeHStar, ArStar)
+            # these show marginal 2D and 1D distributions (aka "corner plot")
+            showCornerPlot3(postCube, Mr1d, FeH1d, Ar1d, mdLocus, xLabel, yLabel, logScale=True, x0=FeHStar, y0=MrStar, z0=ArStar)
+            # Qr vs. FeH posterior and marginal 1D distributions for Qr and FeH
+            Qr1d, margpostQr = showQrCornerPlot(postCube, Mr1d, FeH1d, Ar1d, x0=FeHStar, y0=MrStar, z0=ArStar, logScale=True)
+            catalog['QrEst'][i], catalog['QrEstUnc'][i] = getStats(Qr1d, margpostQr)
+            # basic info 
+            print(' *** 3D Bayes results for star i=', i)
+            print('r mag:', catalog['rmag'][i], 'g-r:', catalog['gr'][i], 'chi2min:', catalog['chi2min'][i])
+            print('Mr: true=', MrStar, 'estimate=', catalog['MrEst'][i], ' +- ', catalog['MrEstUnc'][i])
+            print('FeH: true=', FeHStar, 'estimate=', catalog['FeHEst'][i], ' +- ', catalog['FeHEstUnc'][i])
+            print('Ar: true=', ArStar, 'estimate=', catalog['ArEst'][i], ' +- ', catalog['ArEstUnc'][i])
+            print('Qr: true=', MrStar+ArStar, 'estimate=', catalog['QrEst'][i], ' +- ', catalog['QrEstUnc'][i])
+            print('Mr drop in entropy:', catalog['MrdS'][i])
+            print('FeH drop in entropy:', catalog['FeHdS'][i])
+            print('Ar drop in entropy:', catalog['ArdS'][i])
+
+    # store results
+    writeBayesEstimates(catalog, outfile, iStart, iEnd, do3D=True)
+
+
+def writeBayesEstimates(catalog, outfile, iStart, iEnd, do3D=False):
+    fout = open(outfile, "w")
+    if do3D:
+        fout.write("      glon       glat        FeHEst FeHUnc  MrEst  MrUnc  ArEst  ArUnc  chi2min    MrdS     FeHdS      ArdS \n")
+    else:
+        fout.write("      glon       glat        FeHEst FeHUnc  MrEst  MrUnc  chi2min    MrdS     FeHdS \n")
+    for i in range(iStart, iEnd):
+        r1 = catalog['glon'].iloc[i]
+        r2 = catalog['glat'].iloc[i]
+        r3 = catalog['FeHEst'].iloc[i]
+        r4 = catalog['FeHEstUnc'].iloc[i]
+        r5 = catalog['MrEst'].iloc[i]
+        r6 = catalog['MrEstUnc'].iloc[i]
+        r7 = catalog['chi2min'].iloc[i]
+        r8 = catalog['MrdS'].iloc[i]
+        r9 = catalog['FeHdS'].iloc[i]
+        s = str("%12.8f " % r1) + str("%12.8f  " % r2) + str("%6.2f  " % r3) + str("%5.2f  " % r4)
+        s = s + str("%6.2f  " % r5) + str("%5.2f  " % r6) 
+        if do3D:
+            r15 = catalog['ArEst'].iloc[i]
+            r16 = catalog['ArEstUnc'].iloc[i]
+            r19 = catalog['ArdS'].iloc[i]
+            s = s + str("%6.2f  " % r15) + str("%5.2f  " % r16) + str("%5.2f  " % r7) + str("%8.1f  " % r8)  
+            s = s + str("%8.1f  " % r9) + str("%8.1f  " % r19) + str("%8.0f" % i) + "\n"
+        else:
+            s = s + str("%5.2f  " % r7) + str("%8.1f  " % r8) + str("%8.1f  " % r9) + "\n"
+        fout.write(s)             
+    fout.close()
---
+++ src/photod/priors.py
@@ -0,0 +1,230 @@
+import numpy as np
+import photod.plotting as pt
+
+from scipy.interpolate import griddata
+from scipy.stats import gaussian_kde
+
+
+## given 2D numpy array, make a 3D numpy array by replicating it N3rd times
+## e.g. for prior.shape = (51, 1502) and N3rd=20, returns (51, 1502, 20)
+def make3Dprior(prior, N3rd):
+    return np.repeat(prior[:, :, np.newaxis], N3rd, axis=2)
+
+def readPriors(rootname, locusData, MrColumn='Mr'):
+    # TRILEGAL-based maps were pre-computed for this range...
+    bc = getBayesConstants()
+    rmagMin = bc['rmagMin']
+    rmagMax = bc['rmagMax'] 
+    rmagNsteps = bc['rmagNsteps']  
+    rGrid = np.linspace(rmagMin, rmagMax, rmagNsteps)
+    priors, rmagBinWidth = readPrior(rmagMin, rmagMax, rmagNsteps, rootname)
+    if (bc['rmagBinWidth'] != rmagBinWidth):
+        raise ValueError(f"inconsistency with rmagBinWidth in readPriors (see src/BayesTools.py) {bc['rmagBinWidth']} != {rmagBinWidth}")
+    priorGrid = {}
+    for rind, r in enumerate(rGrid):  
+        # interpolate prior map onto locus Mr-FeH grid 
+        Z = priors[rind]
+        Zval = Z['kde']
+        X = Z['xGrid']
+        Y = Z['yGrid']
+        points = np.array((X.flatten(), Y.flatten())).T
+        values = Zval.flatten()
+        # actual (linear) interpolation
+        priorGrid[rind] = griddata(points, values, (locusData['FeH'], locusData[MrColumn]), method='linear', fill_value=0)
+ 
+    return priorGrid
+
+def readPrior(rmagMin, rmagMax, rmagNsteps,rootname):
+    # read all maps, index by rmag index 
+    rGrid = np.linspace(rmagMin, rmagMax, rmagNsteps)
+    priors = {}
+    for rind, r in enumerate(rGrid):
+        extfile = "-%02d" % (rind)
+        infile = rootname + extfile + '.npz' 
+        priors[rind] = np.load(infile)
+    rmagBinWidth = priors[0]['metadata'][13] # volatile
+    return priors, rmagBinWidth
+
+# get prior map indices for provided array of observed r band mags
+def getPriorMapIndex(rObs):
+    bc = getBayesConstants()
+    rmagMin = bc['rmagMin']
+    rmagMax = bc['rmagMax'] 
+    rmagNsteps = bc['rmagNsteps']  
+    rGrid = np.linspace(rmagMin, rmagMax, rmagNsteps)
+    rind = np.arange(rmagNsteps)
+    zint = np.interp(rObs, rGrid, rind) + bc['rmagBinWidth']
+    return zint.astype(int) 
+
+### numerical model specifications and constants for Bayesian PhotoD method
+def getBayesConstants():
+    BayesConst = {}
+    # parameters for stellar locus (main sequence and red giants) table, as well as priors 
+    BayesConst['FeHmin'] = -2.5       # the range of considered [Fe/H], in priors and elsewhere
+    BayesConst['FeHmax'] = 1.0
+    BayesConst['FeHNpts'] = 36        # defines the grid step for [Fe/H]
+    BayesConst['MrFaint'] = 17.0      # the range of considered Mr, in priors and elsewhere
+    BayesConst['MrBright'] = -2.0 
+    BayesConst['MrNpts'] = 96         # defines the grid step for Mr
+    BayesConst['rmagBinWidth'] = 0.5  # could be dependent on rmag and larger for bright mags...
+    # parameters for pre-computed TRILEGAL-based prior maps
+    BayesConst['rmagMin'] = 14
+    BayesConst['rmagMax'] = 27
+    BayesConst['rmagNsteps'] = 27
+    # parameters for dust extinction grid when fitting 3D (Mr, FeH, Ar)
+    # prior for Ar is flat from (0*Ar+0.0) to (1.3*Ar+0.1) with a step of 0.01 mag ***
+    # where 1.3 <= ArCoef0, 0.1 <= ArCoef1 and 0.01 mag <= ArCoef2 are set here (or user supplied)
+    # ArMin: ArCoeff3*Ar + ArCoeff4 
+    BayesConst['ArCoeff0'] = 1.3
+    BayesConst['ArCoeff1'] = 0.1
+    BayesConst['ArCoeff2'] = 0.01
+    # allow Ar as small as 0
+    BayesConst['ArCoeff3'] = 0.0
+    BayesConst['ArCoeff4'] = 0.0    
+    return BayesConst   
+
+def dumpPriorMaps_testing(sample, fileRootname, pix, show2Dmap=False, verbose=True, NrowMax=200000):
+
+    ## data frame called "sample" here must have the following columns: 'FeH', 'Mr', 'rmag'
+    labels = ['FeH', 'Mr', 'rmag']
+    print("sample", type(sample))
+    ## numerical model specifications and constants for Bayesian PhotoD method
+    bc = getBayesConstants()
+    FeHmin = bc['FeHmin']
+    FeHmax = bc['FeHmax']
+    FeHNpts = bc['FeHNpts']
+    MrFaint = bc['MrFaint']
+    MrBright = bc['MrBright'] 
+    MrNpts = bc['MrNpts']
+    rmagBinWidth = bc['rmagBinWidth']
+    rmagMin = bc['rmagMin']
+    rmagMax = bc['rmagMax'] 
+    rmagNsteps = bc['rmagNsteps']
+
+    # -------
+    metadata = np.array([FeHmin, FeHmax, FeHNpts, MrFaint, MrBright, MrNpts, np.mean(sample['glon']), np.mean(sample['glat']), pix.order, pix.pixel])
+    rGrid = np.linspace(rmagMin, rmagMax, rmagNsteps)
+    # summary file 
+    outsumfile = fileRootname + '-SummaryStats.txt'
+    foutsum = open(outsumfile, "w")
+    foutsum.write(" rMin    rMax   Ntotal     Amin   Amed    Amax     pMS      ppMS     pAGB      pWD        pA1       pA2      pA3      pA4       ptnD     ptkD       pH        pB        pMC \n")
+    print("Healpix: ", pix, "\n---------------------------------------")
+    for rind, r in enumerate(rGrid):
+        # r magnitude limits for this subsample
+        rMin = r - rmagBinWidth
+        rMax = r + rmagBinWidth
+        # select subsample
+        
+        # tS = sample[(sample[labels[2]]>rMin)&(sample[labels[2]]<rMax)]
+
+        # alternative version
+        rfilter = (sample.loc[:, "rmag"]>rMin) & (sample.loc[:, "rmag"]<rMax)
+        tS = sample[rfilter]
+        # print("tS", type(tS))
+
+        if verbose: print('r=', rMin, 'to', rMax, 'N=', len(sample), 'Ns=', len(tS))
+        if (len(tS)>NrowMax):
+            tSmap = tS.sample(n=NrowMax)
+            if verbose: print('subsampled for 2D map to n=', NrowMax)
+        else:
+            tSmap = tS
+
+        # this is work horse, where data are binned and map arrays produced
+        # it takes about 2 mins on Mac OS Apple M1 Pro
+        # so about 70 hours for 2,000 healpix samples 
+        # maps per healpix are about 2 MB, total 4 GB
+        if (len(tSmap) < 3): # Changed to 3 in an attempt to avoid LinAlgErr
+            # print("tSmap", tSmap)
+            print('ERROR: no data to make map (see dumpPriorMaps)')
+            # xGrid, yGrid, Z = 0
+            # xGrid = yGrid = Z = np.zeros(0) # <-- ispravio, bolje bi bilo staviti pass
+            # pass  # <-- dodao pass. još bolje bi bilo dodati ga iznad.
+        else:
+            xGrid, yGrid, Z = get2Dmap(tSmap, labels, metadata)
+            # display for sanity tests, it can be turned off
+            if show2Dmap:
+                pt.show2Dmap(xGrid, yGrid, Z, metadata, labels[0], labels[1], logScale=True)
+            # store this particular map (given healpix and r band magnitude slice) 
+            extfile = "-%02d" % (rind)     # re.findall(r'\d+', input_string)
+            # it is assumed that fileRootname knows which healpix is being processed,
+            # as well as the magnitude range specified by rmagMin and rmagMax
+            outfile = fileRootname + extfile + '.npz' 
+            Zrshp = Z.reshape(xGrid.shape)
+            tSsize = np.size(tS) # Total size of array: rows*cols
+            mdExt = np.concatenate((metadata, np.array([rmagMin, rmagMax, rmagNsteps, rmagBinWidth, tSsize, r])))
+            np.savez(outfile, xGrid=xGrid, yGrid=yGrid, kde=Zrshp, metadata=mdExt, labels=labels)
+            ## summary info
+            s1 = str("%5.1f  " % rMin) + str("%5.1f  " % rMax) + str("%10.0f  " % len(tS)) # Ntotal!
+            # dust extinction information
+            A1 = np.min(tS['Av'])
+            A2 = np.median(tS['Av'])
+            A3 = np.max(tS['Av'])
+            s2 = str("%6.2f " % A1) + str("%6.2f " % A2) + str("%6.2f " % A3) 
+            # evolutionary stats
+            df = tS
+            dfTms = df[df['label']==1]
+            dfTpms = df[(df['label']>1)&(df['label']<7)] # according to TRILEGAL labels column these would be SGB, RGB, CHeB (4,5,6). Nomenclature is confusing because "pms" usually corresponds to pre-main sequence. Given the cuts, this would rather apply to post main sequence.
+            dfTagb = df[(df['label']>6)&(df['label']<9)] # EAGBs & TPAGBs
+            dfTwd = df[df['label']==9] # PAGBs + WDs
+            Tms = len(dfTms)
+            Tpms = len(dfTpms)
+            Tagb = len(dfTagb)
+            Twd = len(dfTwd)
+            # probabilities
+            Ttotal = Tms + Tpms + Tagb + Twd
+            if (Ttotal>0):
+                pms = Tms/Ttotal
+                ppms = Tpms/Ttotal
+                pagb = Tagb/Ttotal    
+                pwd = Twd/Ttotal
+            else: pms = ppms = pagb = pwd = -1
+            s3 = str("%8.3e " % pms) + str("%8.3e " % ppms) + str("%8.3e " % pagb) + str("%8.3e " % pwd)
+            # age distribution
+            dfA1 = df[df['logage']<7]
+            dfA2 = df[df['logage']<8]
+            dfA3 = df[df['logage']<9]
+            dfA4 = df[df['logage']<10] 
+            Atotal = len(df)
+            pA1 = len(dfA1)/Atotal
+            pA2 = len(dfA2)/Atotal
+            pA3 = len(dfA3)/Atotal
+            pA4 = len(dfA4)/Atotal
+            s4 = str("%8.3e " % pA1) + str("%8.3e " % pA2) + str("%8.3e " % pA3) + str("%8.3e " % pA4)  
+            # galactic components
+            dfC1 = df[df['comp']==1]
+            dfC2 = df[df['comp']==2]
+            dfC3 = df[df['comp']==3]
+            dfC4 = df[df['comp']==4]
+            dfC5 = df[df['comp']==5]
+            Ctotal = len(df)
+            pC1 = len(dfC1)/Ctotal
+            pC2 = len(dfC2)/Ctotal
+            pC3 = len(dfC3)/Ctotal
+            pC4 = len(dfC4)/Ctotal
+            pC5 = len(dfC5)/Ctotal
+            s5 = str("%8.3e " % pC1) + str("%8.3e " % pC2) + str("%8.3e " % pC3) + str("%8.3e " % pC4) + str("%8.3e " % pC5)
+            s = s1 + s2 + s3 + s4 + s5 + "\n"
+            foutsum.write(s)
+    foutsum.close()
+
+def get2Dmap(sample, labels, metadata):
+    data = np.vstack([sample[labels[0]], sample[labels[1]]])
+    try:
+        kde = gaussian_kde(data)     
+        # evaluate on a regular grid
+        xMin = metadata[0] # FeHmin
+        xMax = metadata[1] # FeHMax
+        nXbin = int(metadata[2]) # FeHNpts ??? why
+        yMin = metadata[3]
+        yMax = metadata[4]
+        nYbin = int(metadata[5])
+        xgrid = np.linspace(xMin, xMax, nXbin)
+        ygrid = np.linspace(yMin, yMax, nYbin)
+        Xgrid, Ygrid = np.meshgrid(xgrid, ygrid)
+        Z = kde.evaluate(np.vstack([Xgrid.ravel(), Ygrid.ravel()]))
+        return (Xgrid, Ygrid, Z)
+    except:
+        # print("LinAlgError", metadata)
+        print("Error", metadata)
+        print(metadata)
+        pass
