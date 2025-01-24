0265c5a87a2253c437d50a4f0ff9344871c3e402
Derek T. Jones <dtj@mac.com>
2025-01-24 08:54:47 -0800

    Add the map-creating function

--- src/photod/column_map/base.py
+++ src/photod/column_map/base.py
@@ -3,6 +3,8 @@ Base class of any column map.
 """
 
 from dataclasses import dataclass
+from pathlib import Path
+import yaml
 
 
 @dataclass
@@ -14,3 +16,17 @@ class ColumnMap:
     def __init__(self, name: str, purpose: str):
         self.name = name
         self.purpose = purpose
+
+
+def mapper_from_glossary(class_name: str, purpose: str, glossary_yaml: Path) -> ColumnMap:
+    """
+    Given a class name and purpose, creates a class of that name as a specialization of
+    ColumnMap whose attributes are read-only properties informed by the elements of the
+    glossary that specify a variable and an id.
+    """
+    with open(glossary_yaml) as f_in:
+        glossary = yaml.safe_load(f_in)
+    clazz_attrs = {'__init__': lambda self: ColumnMap.__init__(self, class_name, purpose)}
+    clazz_attrs.update({x['variable']: property(fget=lambda s, value=x['id']: value, doc=x['description']) for x in glossary if 'variable' in x})
+    clazz = type(class_name, (ColumnMap,), clazz_attrs)
+    return clazz()
--- src/photod/column_map/catalog.py
+++ src/photod/column_map/catalog.py
@@ -2,58 +2,9 @@
 Column map for catalog data.
 """
 
-from dataclasses import dataclass
-from photod.column_map import base
+from photod.column_map.base import mapper_from_glossary
+from pathlib import Path
 
+glossary_path = Path(__file__).parent / 'glossary.yaml'
+m = mapper_from_glossary("CatalogColumnMap", "Reference catalog columns", glossary_path)
 
-@dataclass
-class ColumnMap(base.ColumnMap):
-
-    def __init__(self):
-        super().__init__("Catalog column map", "Reference catalog columns")
-
-    # Est suffix = estimate
-    # Unc suffix = uncertainty
-    # EstUnc = estimated uncertainty as a +/- delta for the
-    #     quantity it modifies (confidence interval)
-    # dS suffix: drop in entropy
-
-    # ##### Extinction in r band
-    extinction_r: str = "Ar"
-    extinction_r_est = "ArEst"
-    extinction_r_est_unc = "ArEstUnc"
-
-    extinction_r_entropy_drop = "ArdS"
-
-    # ##### Metallicity
-    metallicity: str = "FeH"
-    metallicity_est: str = "FeHEst"
-    metallicity_est_unc: str = "FeHEstUnc"
-
-    metallicity_entropy_drop: str = "FeHdS"
-
-    # ##### Absolute magnitude, r band
-
-    abs_mag_r: str = "Mr"
-    abs_mag_r_est: str = "MrEst"
-    abs_mag_r_est_unc: str = "MrEstUnc"
-
-    abs_mag_r_entropy_drop: str = "MrdS"
-
-    # ##### Absolute magnitude plus extinction, r band
-
-    abs_mag_ext_r: str = "Qr"
-    abs_mag_ext_r_est: str = "QrEst"
-    abs_mag_ext_r_est_unc: str = "QrEstUnc"
-
-    observed_mag_r: str = "rmag"
-
-    galactic_latitude: str = "glat"
-    galactic_longitude: str = "glon"
-
-    chi_sq_min: str = "chi2min"
-
-    g_minus_r: str = "gr"
-
-
-m = ColumnMap()
